0a65dc426056e016bb79120491c282708feda264
NicolasDomingos09 <nicolasdomingos149@gmail.com>
2024-08-17 23:31:22 -0300

    abs

--- mat.py
+++ mat.py
@@ -1,4 +1,4 @@
-num1 = int(input("Entre com o primeiro número "))
+num1 = int(input("Entre com o primeiro número: "))
 num2 = int(input("Entre com o segundo número: "))
 
 op = input("Qual operação matemática será realizada? ")
@@ -6,12 +6,14 @@ op = input("Qual operação matemática será realizada? ")
 if op == '+':
     print(num1 + num2)
 elif op == '-':
-    print(num1 - num2)
+    print(abs(num1 - num2))
 elif op == '*':
     print(num1 * num2)
 elif op == '/':
     if num2 != 0:
         print(num1 / num2)
+    else:
+        print("Não é possível dividir por zero")
 else:
     print("Operação inválida")
         
